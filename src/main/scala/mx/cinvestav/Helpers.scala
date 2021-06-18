6cd5278f34bf60a43711f3425e815df79a4be34f
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2021-06-17 20:00:05 -0500

    [refactor]

--- src/main/scala/mx/cinvestav/Helpers.scala
+++ src/main/scala/mx/cinvestav/Helpers.scala
@@ -63,35 +63,41 @@ object Helpers {
       }
 
 
+  def sendCoordinatorCmd(lowest:List[(String, String=>IO[Unit])],state:Ref[IO,NodeState],_signal:SignallingRef[IO,Boolean])(implicit
+                                                                                       utils: RabbitMQUtils[IO],
+                                                                                      logger: Logger[IO])
+  = {
+    for {
+      _              <- Logger[IO].debug(s"NOW IM THE F*CK LEADER!  =>  ${config.shadowLeader} - ${config.node}")
+      previousState  <- state.getAndUpdate(_.copy(isLeader = true,status = status.Up,leader = config.node, shadowLeader = config.nodeId))
+      //        Send stop heart command
+      previousLeaderPub <-  utils.createPublisher(config.poolId,s"${config.poolId}.${previousState.leader}.default")
+      stopCmd           <- CommandData[Json](Identifiers.STOP_HEARTBEAT,Json.Null).asJson.noSpaces.pure[IO]
+      _                 <- previousLeaderPub(stopCmd)
+      // Start heart in new leader node
+      currentLeaderPub <-  utils.createPublisher(config.poolId,s"${config.poolId}.${config.node}.default")
+      startCmd          <- CommandData[Json](Identifiers.START_HEARTBEAT,Json.Null).asJson.noSpaces.pure[IO]
+      _                 <- currentLeaderPub(startCmd)
+      //
+      _              <- _signal.set(true)
+      pubs           <- lowest.map(_._2).pure[IO]
+      coordinatorCmd <- CommandData[Json](Identifiers.COORDINATOR,Payloads.Coordinator(config.node,config.nodeId).asJson)
+        .asJson.noSpaces.pure[IO]
+      _              <- pubs.traverse(p=>p(coordinatorCmd))
+      _              <- Logger[IO].debug("SEND COORDINATOR COMMAND")
+
+    } yield ()
+  }
   def electionBackgroundTask(
                               state:Ref[IO,NodeState],
                               currentState:NodeState,
                               highest:List[(String,String=>IO[Unit])],
-                              lowest:List[(String, String=>IO[Unit])])(implicit utils:RabbitMQUtils[IO],
+                              lowest:List[(String, String=>IO[Unit])]
+                            )(implicit utils:RabbitMQUtils[IO],
                                                                        logger: Logger[IO]): IO[Unit]
   = {
-    def sendCoordinatorCmd(_signal:SignallingRef[IO,Boolean]) = {
-      for {
-        _              <- Logger[IO].debug(s"NOW IM THE F*CK LEADER!  =>  ${config.shadowLeader} - ${config.node}")
-        previousState  <- state.getAndUpdate(_.copy(isLeader = true,status = status.Up,leader = config.node, shadowLeader = config.nodeId))
-        //        Send stop heart command
-        previousLeaderPub <-  utils.createPublisher(config.poolId,s"${config.poolId}.${previousState.leader}.default")
-        stopCmd           <- CommandData[Json](Identifiers.STOP_HEARTBEAT,Json.Null).asJson.noSpaces.pure[IO]
-        _                 <- previousLeaderPub(stopCmd)
-        // Start heart in new leader node
-        currentLeaderPub <-  utils.createPublisher(config.poolId,s"${config.poolId}.${config.node}.default")
-        startCmd          <- CommandData[Json](Identifiers.START_HEARTBEAT,Json.Null).asJson.noSpaces.pure[IO]
-        _                 <- currentLeaderPub(startCmd)
-        //
-        _              <- _signal.set(true)
-        pubs           <- lowest.map(_._2).pure[IO]
-        coordinatorCmd <- CommandData[Json](Identifiers.COORDINATOR,Payloads.Coordinator(config.node,config.nodeId).asJson)
-          .asJson.noSpaces.pure[IO]
-        _              <- pubs.traverse(p=>p(coordinatorCmd))
-        _              <- Logger[IO].debug("SEND COORDINATOR COMMAND")
 
-      } yield ()
-    }
+
     for {
     signal <- currentState.electionSignal.pure[IO]
     _      <- signal.set(false)
@@ -101,7 +107,7 @@ object Helpers {
             currentState <- state.get
             _signal      <- currentState.electionSignal.pure[IO]
             okMessages   <- currentState.okMessages.pure[IO]
-            _            <- if(x >= 15 && okMessages.nonEmpty) sendCoordinatorCmd(_signal) else IO.unit
+            _            <- if(x >= 15 && okMessages.nonEmpty) sendCoordinatorCmd(lowest,state,_signal) else IO.unit
             _            <- Logger[IO].debug(s"OK MESSAGES[Retries = $x, okMessages = ${okMessages.length}]! - ${highest.length}")
 //            _            <- IO.println(okMessages)
             _            <- if(okMessages.isEmpty)  _signal.set(true) else IO.unit
@@ -115,8 +121,22 @@ object Helpers {
     def sendCmd(publishers:List[String=>IO[Unit]],commandData: CommandData[Json]) = for {
       res          <- publishers.traverse(p=>p(commandData.asJson.noSpaces))
     } yield ()
-    def doBully() = for {
+    def doBully(currentState:NodeState, highestIdPubs:List[(String,String=>IO[Unit])], lowestIdPubs:List[(String, String=>IO[Unit])]) = for {
 
+      electionCmd  <- CommandData[Json](Identifiers.ELECTIONS,Payloads.Election(config.node,config.nodeId).asJson).pure[IO]
+      //      SAVE OK MESSAGES
+      //      _           <- state.update(s=>s.copy(okMessages = highestNodes.indices.toList))
+      highestNodes <- highestIdPubs.map(_._1).pure[IO]
+      highestPubs <- highestIdPubs.map(_._2).pure[IO]
+      _           <- state.update(s=>s.copy(okMessages = highestNodes))
+      //    Lowest nodes
+      /*/
+         Now process P sends election message to every process with high priority number.
+         if high priority nodes list is empty
+       */
+      _  <- electionBackgroundTask(state,currentState,highestIdPubs,lowestIdPubs).start
+      _  <- sendCmd(highestPubs,electionCmd)
+      //      _            <- if(highestNodes.isEmpty) IO.println("SEND COORDINATOR") else sendCmd(highestPubs,electionCmd)
     } yield ()
 
     for {
@@ -124,28 +144,21 @@ object Helpers {
       currentState <- state.updateAndGet(_.copy(status= statusx.BullyStatus.Election))
       //   Highest nodes
       highestNodes <- currentState.bullyNodes.filter(_.priority>config.priority).map(_.nodeId).pure[IO]
-      _            <- if(highestNodes.isEmpty) IO.println("COORDINATOR") else IO.println("DO BULLY")
       highestPubs  <- highestNodes.traverse(Helpers.createBullyNode)
-      electionCmd  <- CommandData[Json](Identifiers.ELECTIONS,Payloads.Election(config.node,config.nodeId).asJson).pure[IO]
-//      SAVE OK MESSAGES
-//      _           <- state.update(s=>s.copy(okMessages = highestNodes.indices.toList))
-      _           <- state.update(s=>s.copy(okMessages = highestNodes))
-//    Lowest nodes
       lowestNodes   <- currentState.bullyNodes
         .filter(_.priority < config.priority)
         .map(_.nodeId).pure[IO].map(_:+currentState.shadowLeader).map(_.toSet.toList)
       lowestPubs    <- lowestNodes.traverse(Helpers.createBullyNode)
       _            <- Logger[IO].debug(s"HIGHEST: $highestNodes")
       _            <- Logger[IO].debug(s"LOWEST: $lowestNodes")
-      /*/
-         Now process P sends election message to every process with high priority number.
-         if high priority nodes list is empty
-       */
       highestIdPubs <- highestNodes.zip(highestPubs).pure[IO]
       lowestIdPubs <- lowestNodes.zip(lowestPubs).pure[IO]
-      _  <- electionBackgroundTask(state,currentState,highestIdPubs,lowestIdPubs).start
-      _  <- sendCmd(highestPubs,electionCmd)
-//      _            <- if(highestNodes.isEmpty) IO.println("SEND COORDINATOR") else sendCmd(highestPubs,electionCmd)
+      _signal      <- state.get.map(_.electionSignal)
+
+      _            <- if(highestNodes.isEmpty)
+                      Helpers.sendCoordinatorCmd(lowestIdPubs,state,_signal)
+                    else
+                      doBully(currentState,highestIdPubs,lowestIdPubs)
     } yield ()
   }
 
